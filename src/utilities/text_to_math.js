09ec217a158c432c4c8158140e3302f4d61eed27
brentanalexander <brentan.alexander@gmail.com>
2015-10-12 16:06:01 -0700

    Initial commit of text_to_math, which needs to one day be written

---
+++ src/utilities/text_to_math.js
@@ -0,0 +1,9 @@
+/*
+Library will take textual representations of mathematical expressions
+such as "c = x^2 + 2/5" and convert into mathquill syntax.
+
+Based on MathJS parser by Jos de Jong
+*/
+
+// BRENTAN: WRITE ME!!
+
