490d2cdcd8f7eac2a8961c41f2d0d3ce169adc24
brentanalexander <brentan.alexander@gmail.com>
2015-12-03 12:09:10 -0800

    Remove IE debug line

--- src/wrapper/outro.js
+++ src/wrapper/outro.js
@@ -36,4 +36,3 @@
 		elements[giac_elements_to_add[i].key] = SwiftCalcs.elements[giac_elements_to_add[i].key] = giac_elements_to_add[i].el;
 }());
 window.SwiftCalcs_js_ready = true;
-console.log('SWIFT: ' + window.SwiftCalcs_js_ready);
